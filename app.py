0cec3863c29af2b774bc7ce8c5f624b39f30b580
Saefullah <saefullah.kamaruddin@ucdconnect.ie>
2020-05-07 16:57:31 +0800

    change jwt lifetime

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import config
 from flask import Flask
 from models.base_model import db
 from flask_wtf.csrf import CSRFProtect
+import datetime
 
 
 web_dir = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), 'instagram_web')
 
 app = Flask('NEXTAGRAM', root_path=web_dir)
-
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
 if os.getenv('FLASK_ENV') == 'production':
     app.config.from_object("config.ProductionConfig")
 else:
--- instagram_api/utils/payment.py
+++ instagram_api/utils/payment.py
@@ -50,24 +50,18 @@ def checkout():
             "submit_for_settlement": True
         }
     })
-    return "Temp"
-   # if payment is successful , update payment status to true
-   # payment.save total amount,   Braintree_Transaction_id , get by id = user
-    # if result.is_success:
-    #     current_id = User.get_by_id(get_jwt_identity())
-    #     Payment(user=current_id, Braintree_Transaction_id=result.transaction.id,
-    #             Total_amount=result.transaction.amount).save()
 
-    #     Cart.update(payment_status=True).where(
-    #         Cart.user == current_id, Cart.payment_status == False).execute()
+    current_id = User.get_by_id(get_jwt_identity())
+    Payment(user=current_id, Braintree_Transaction_id=result.transaction.id,
+            Total_amount=result.transaction.amount).save()
 
-    #     print(result.transaction)
-    #     print(result.transaction.id)
-    #     print(result.transaction.amount)
+    Cart.update(payment_status=True).where(
+        Cart.user == current_id, Cart.payment_status == False).execute()
 
-    #     send_after_payment(current_id.email)
+    print(result.transaction)
+    print(result.transaction.id)
+    print(result.transaction.amount)
+    send_after_payment(current_id.email)
 
-    #     return jsonify({'message': 'Success', 'Amount Paid': (result.transaction.amount), 'status': 'failed'}), 200
+    return jsonify({'message': 'Success', 'Amount Paid': (result.transaction.amount), 'status': 'success'}), 200
 
-    # else:
-    return jsonify({'message': 'failed', 'status': 'failed'}), 400
