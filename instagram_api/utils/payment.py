5aeeaa38621e740fba09164307e6b7595e138c33
Saefullah <saefullah.kamaruddin@ucdconnect.ie>
2020-05-09 04:15:29 +0800

    mail + cart update

--- instagram_api/utils/payment.py
+++ instagram_api/utils/payment.py
@@ -40,45 +40,65 @@ def new_payment():
 @csrf.exempt
 @jwt_required
 def checkout():
-
     current_id = User.get_by_id(get_jwt_identity())
+    not_enough_items = Cart.select().join(Item).where(Cart.user == current_id,
+                                                      Cart.payment_status == False, Cart.amount > Item.stock)
 
-    print(request.form.get('paymentMethodNonce'))
-    data = request.get_json()
-    amount_input = data['amount']
-    pmNonce_input = data['paymentMethod']
+    if len(not_enough_items) > 0:
+        return jsonify("Message: There are not enough of these items in stock",
+                       [{"item":
+                         {"id": item.item.id,
+                          "stock": item.item.stock,
+                          "color": item.item.color,
+                          "name": item.item.name,
+                          "product_type": item.item.product_type,
+                          "image": item.item.image_url,
+                          "price": item.item.price,
+                          "size": item.item.size}}
+                           for item in not_enough_items], "Please reduce amount", {"Status": "Failed"}), 400
 
-    result = gateway.transaction.sale({
-        "amount": amount_input,
-        "payment_method_nonce": pmNonce_input,
-        "options": {
-            "submit_for_settlement": True
-        }
-    })
+    else:
+        print(request.form.get('paymentMethodNonce'))
+        data = request.get_json()
+        amount_input = data['amount']
+        pmNonce_input = data['paymentMethod']
 
-    Payment(user=current_id, Braintree_Transaction_id=result.transaction.id,
-            Total_amount=result.transaction.amount).save()
+        result = gateway.transaction.sale({
+            "amount": amount_input,
+            "payment_method_nonce": pmNonce_input,
+            "options": {
+                "submit_for_settlement": True
+            }
+        })
 
-    Cart.update(payment_status=True).where(
-        Cart.user == current_id, Cart.payment_status == False).execute()
+        Payment(user=current_id, Braintree_Transaction_id=result.transaction.id,
+                Total_amount=result.transaction.amount).save()
 
-    print(result.transaction)
-    print(result.transaction.id)
-    print(result.transaction.amount)
-    send_after_payment(current_id.email)
+        Cart.update(payment_status=True).where(
+            Cart.user == current_id, Cart.payment_status == False).execute()
 
-    return jsonify({'message': 'Success', 'Amount Paid': (result.transaction.amount), 'status': 'success'}), 200
+        print(result.transaction)
+        print(result.transaction.id)
+        print(result.transaction.amount)
+        send_after_payment(current_id.email)
+
+        return jsonify({'message': 'Success', 'Amount Paid': (result.transaction.amount), 'status': 'success'}), 200
 
 
-@payment_api_blueprint.route('/checkout', methods=['GET'])
+@payment_api_blueprint.route('/test', methods=['POST'])
 @csrf.exempt
 @jwt_required
 def match():
     current_id = User.get_by_id(get_jwt_identity())
-    # carts = Cart.select().where(
-    #     Cart.user == current_id, Cart.payment_status == False)
-    not_enough_items = Cart.select().join(Item).where(Cart.user == current_id,
-                                                      Cart.payment_status == False, Cart.amount > Item.stock)
+    carts = Cart.select().where(
+        Cart.user == current_id, Cart.payment_status == False)
+    # not_enough_items = Cart.select().join(Item).where(Cart.user == current_id,
+    #                                                   Cart.payment_status == False, Cart.amount > Item.stock)
+
+    # print(len(not_enough_items))
+
+    Cart.update({Cart.item.stock: (Cart.item.stock-Cart.amount)}).where(Cart.user == current_id,
+                                                                        Cart.payment_status == False).execute()  # Execute the query, returning number of rows updated.
 
     # print(([{
     #     "id": cart.user.id,
@@ -89,24 +109,45 @@ def match():
     # print([cart.amount for cart in carts])
     # print([cart.item.stock for cart in carts])
 
-    # amount = [cart.amount for cart in carts]
-    # stock = [cart.item.stock for cart in carts]
+    # amount_bought = [cart.amount for cart in carts]
+    # stock_remainder = [cart.item.stock for cart in carts]
+    # remainder = list(map(operator.sub, stock_remainder, amount_bought))
 
-    return jsonify([{"user": {
-                    "id": item.user.id,
-                    "username": item.user.username},
-        "cart": {"id": item.id,
-                 "amount": item.amount},
-        "item":
-        {"id": item.item.id,
-         "stock": item.item.stock,
-         "color": item.item.color,
-         "name": item.item.name,
-         "product_type": item.item.product_type,
-         "image": item.item.image_url,
-         "price": item.item.price,
-         "size": item.item.size}}
-        for item in not_enough_items])
+    # cart_afterpay.update(Cart.item.stock == (Cart.item.stock - Cart.amount)
+    #                      ).where(Cart.user == cart_afterpay, Cart.payment_status == False).execute()
+
+    return jsonify([{"id": cart.user.id,
+                     "amount": cart.amount,
+                     "item id": cart.item.id,
+                     "stock": cart.item.stock}for cart in carts])
+
+    # return jsonify([{"user": {
+    #                 "id": item.user.id,
+    #                 "username": item.user.username},
+    #     "cart": {"id": item.id,
+    #              "amount": item.amount},
+    #     "item":
+    #     {"id": item.item.id,
+    #      "stock": item.item.stock,
+    #      "color": item.item.color,
+    #      "name": item.item.name,
+    #      "product_type": item.item.product_type,
+    #      "image": item.item.image_url,
+    #      "price": item.item.price,
+    #      "size": item.item.size}}
+    #     for item in not_enough_items])
+
+    # return jsonify("Message: There are not enough of these items in stock",
+    #                [{"item":
+    #                  {"id": item.item.id,
+    #                   "stock": item.item.stock,
+    #                   "color": item.item.color,
+    #                   "name": item.item.name,
+    #                   "product_type": item.item.product_type,
+    #                   "image": item.item.image_url,
+    #                   "price": item.item.price,
+    #                   "size": item.item.size}}
+    #                    for item in not_enough_items], "Please reduce amount", {"Status": "Failed"}), 400
 
     # remainder = list(map(operator.sub, stock, amount))
     # return jsonify(print(remainder))
