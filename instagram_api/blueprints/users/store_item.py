8fc8bd4a30de12a8b9e2f586e1665c9b8cebb42e
Saefullah <saefullah.kamaruddin@ucdconnect.ie>
2020-05-09 16:14:51 +0800

    fix item api

--- instagram_api/blueprints/users/store_item.py
+++ instagram_api/blueprints/users/store_item.py
@@ -41,7 +41,7 @@ def get_distinct():
 def get_all_item():
     items = Item.select()
 
-    return jsonify([{"id": item.id"name": item.name, "type": item.product_type,
+    return jsonify([{"id": item.id, "name": item.name, "type": item.product_type,
                      "size": item.size, "color": item.color, "price": item.price, "image": item.image, "stock": item.stock, "image_url": item.image_url} for item in items])
 
 
